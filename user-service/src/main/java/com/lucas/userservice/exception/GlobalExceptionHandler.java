25bff73ad3cb4cd73ecf07522a1e9af623c5ac46
Lucas <lg1352439@gmail.com>
2026-08-11 16:15:43 -0300

    refactor: extend ResponseEntityExceptionHandler

--- user-service/src/main/java/com/lucas/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/lucas/userservice/exception/GlobalExceptionHandler.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private ProblemDetail createProblemDetail(
             HttpStatus status,
