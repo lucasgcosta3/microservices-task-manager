2e580deb5bb80528bc8200d545e680f52ba603bd
Lucas <lg1352439@gmail.com>
2026-08-11 16:16:32 -0300

    fix: rename user entity table to avoid postgresql reserved keyword

--- user-service/src/main/java/com/lucas/userservice/entity/User.java
+++ user-service/src/main/java/com/lucas/userservice/entity/User.java
@@ -6,6 +6,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -29,13 +30,17 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private boolean active;
+    private Boolean active;
 
     private LocalDateTime createdAt;
 
     @PrePersist
     public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.active = true;
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+        if (this.active == null) {
+            this.active = true;
+        }
     }
 }
