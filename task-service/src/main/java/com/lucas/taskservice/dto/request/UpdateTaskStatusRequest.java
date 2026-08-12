cf3fe667d57bfc9cb479ebd8962fb1aa047bdb01
Lucas <lg1352439@gmail.com>
2026-08-12 17:54:19 -0300

    fix: switched NotBlank for NotNull

--- task-service/src/main/java/com/lucas/taskservice/dto/request/UpdateTaskStatusRequest.java
+++ task-service/src/main/java/com/lucas/taskservice/dto/request/UpdateTaskStatusRequest.java
@@ -1,7 +1,7 @@
 package com.lucas.taskservice.dto.request;
 
 import com.lucas.taskservice.entity.TaskStatus;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateTaskStatusRequest(
         @NotNull(message = "The status is required")
